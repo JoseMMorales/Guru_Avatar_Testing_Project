1ffec2a32682f8e1102749408ecb817863763b9e
Jose <josemulamorales33@hotmail.com>
2020-02-02 20:27:58 +0000

    Selenium, Java Scripts

---
+++ Guru_Avatar_Scripts/src/room_module/TC12.java
@@ -0,0 +1,85 @@
+package room_module;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC12 {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "/......./chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.navigate().to("https://www.myguruavatar.com/demo/");
+		
+		driver.manage().timeouts().implicitlyWait(2 , TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+		WebElement Username = driver.findElement(By.xpath("//*[@id=\"modlgn-username\"]"));
+		WebElement Password = driver.findElement(By.xpath("//*[@id=\"modlgn-passwd\"]"));
+		WebElement Login = driver.findElement(By.xpath("//*[@id=\"hmelogin\"]"));
+		
+		if(Username.isDisplayed() & Username.isEnabled()){
+			
+			Username.sendKeys("josemulamorales33@hotmail.com");
+		}
+		
+		if(Password.isDisplayed() & Password.isEnabled()) {
+			
+			Password.sendKeys("Jose Morales");
+		}
+	
+		if(driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input")).isSelected()==false)
+				{
+					driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input")).click();
+				}
+			
+		if(Login.isDisplayed() && Login.isEnabled()) {
+			
+			Login.click();
+		}
+			
+		Thread.sleep(3000);
+		
+		WebElement Switch = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]"));
+		WebElement Goto = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]/option[2]"));
+		
+		if(Switch.isDisplayed() && Switch.isEnabled()) {
+			
+			Switch.click();
+		}
+		
+		if(Goto.isEnabled() && Goto.isDisplayed()) {
+			
+			Goto.click();
+		}
+		
+		Thread.sleep(2000); 
+		
+		WebElement Edit = driver.findElement(By.xpath("//*[@id=\"ja-content-main\"]/div[2]/div[2]/div/div/a"));
+			
+		Edit.click();
+		
+		if(driver.getPageSource().contains("Edit Your Profile"))
+		{
+			
+			System.out.println("Test Passed!");
+		}
+		else {
+			
+			System.out.println("Test Failed!");
+			
+		}
+		
+		driver.quit();
+		
+	}
+	
+
+}
---
+++ Guru_Avatar_Scripts/src/room_module/TC7.java
@@ -0,0 +1,66 @@
+package room_module;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC7 {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/........./chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.myguruavatar.com/demo/");
+		
+		driver.manage().timeouts().implicitlyWait( 2, TimeUnit.SECONDS);
+		
+		driver.manage().window().maximize();
+		
+		WebElement Username = driver.findElement(By.xpath("//*[@id=\"modlgn-username\"]"));
+		WebElement Password = driver.findElement(By.xpath("//*[@id=\"modlgn-passwd\"]"));
+		WebElement Login = driver.findElement(By.xpath("//*[@id=\"hmelogin\"]"));
+		
+		if(Username.isDisplayed() & Username.isEnabled()) {
+			
+			Username.sendKeys("josemulamorales33@hotmail.com");
+		}
+		
+		if(Password.isDisplayed() & Password.isEnabled()) {
+			
+			Password.sendKeys("Jose Morales");
+		}
+		
+		if(driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input")).isSelected()==false)
+			{
+				driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input")).click();
+			}
+		
+		if(Login.isDisplayed() & Login.isEnabled()) {
+			
+			Login.click();
+		}
+
+		Thread.sleep(3000);
+		
+		WebElement Switch = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]"));
+		Switch.click();
+		
+		WebElement Goto = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]/option[2]"));
+		Goto.click();
+		
+		Thread.sleep(3000); 
+		
+		String text = driver.findElement(By.xpath("//*[@id=\"menu2_section\"]/div/div/div[2]/p")).getText();
+		
+		System.out.println(text);
+		
+		driver.close();
+
+	}
+
+}
