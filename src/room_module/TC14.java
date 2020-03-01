c3024de7f921fbf953cd84b8f6465eb2f2e7ef10
Jose <josemulamorales33@hotmail.com>
2020-03-01 18:23:00 +0000

    Scripts built for automation testing

--- src/room_module/TC14.java
+++ src/room_module/TC14.java
@@ -1,76 +1,60 @@
 package room_module;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class TC14 {
-	
-	static WebDriver driver;
+  
+  static WebDriver driver;
 
-	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/.........../chromedriver");
-		driver = new ChromeDriver();
-		
-		driver.navigate().to("https://www.myguruavatar.com/demo/");
-		
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-		
-		driver.manage().window().fullscreen();
-		
-		WebElement Username = driver.findElement(By.xpath("//*[@id=\"modlgn-username\"]"));
-		WebElement Password = driver.findElement(By.xpath("//*[@id=\"modlgn-passwd\"]"));
-		
-		if(Username.isDisplayed() && Username.isEnabled()){
-			
-			Username.sendKeys("josemulamorales33@hotmail.com");
-		}
-		
-		if(Password.isDisplayed() && Password.isEnabled()) {
-			
-			Password.sendKeys("Jose Morales");	
-		}
-		
-		WebElement Checkbutton = driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input"));
-		Checkbutton.click();
-	
-		WebElement Login = driver.findElement(By.xpath("//*[@id=\"hmelogin\"]"));
-		Login.click();
-		
-		Thread.sleep(3000);
-		
-		WebElement Switch = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]"));
-		Switch.click();
-		
-		WebElement Goto = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]/option[2]"));
-		Goto.click();
-		
-		Thread.sleep(2000);
-		
-		WebElement Like = driver.findElement(By.xpath("//*[@id=\"1293\"]"));
-		
-		Like.click();	
-		
-		Thread.sleep(2000);
-        		   	
-		if(driver.getPageSource().contains("Pofile Likes Detail"))
-		{
-			
-			System.out.println("Test Passed!");
-		}
-		else {
-			
-			System.out.println("Test Failed!");
-			
-		}
-		
-		driver.close();
-	}
-
-	
-
+  public static void main(String[] args) throws Exception {
+    System.setProperty("webdriver.chrome.driver", "/.........../chromedriver");
+    driver = new ChromeDriver();
+    driver.navigate().to("https://www.myguruavatar.com/demo/");
+    driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    driver.manage().window().fullscreen();
+    
+    WebElement Username = driver.findElement(By.xpath("//*[@id=\"modlgn-username\"]"));
+    WebElement Password = driver.findElement(By.xpath("//*[@id=\"modlgn-passwd\"]"));
+    
+    if(Username.isDisplayed() && Username.isEnabled()){
+      Username.sendKeys("josemulamorales33@hotmail.com");
+    }
+    if(Password.isDisplayed() && Password.isEnabled()) {
+      Password.sendKeys("Jose Morales");	
+    }
+    
+    WebElement Checkbutton = driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input"));
+    Checkbutton.click();
+  
+    WebElement Login = driver.findElement(By.xpath("//*[@id=\"hmelogin\"]"));
+    Login.click();
+    
+    Thread.sleep(3000);
+    
+    WebElement Switch = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]"));
+    Switch.click();
+    
+    WebElement Goto = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]/option[2]"));
+    Goto.click();
+    
+    Thread.sleep(2000);
+    
+    WebElement Like = driver.findElement(By.xpath("//*[@id=\"1293\"]"));
+    Like.click();	
+    
+    Thread.sleep(2000);
+                 
+    if(driver.getPageSource().contains("Pofile Likes Detail")) {
+      System.out.println("Test Passed!");
+    }
+    else {
+      System.out.println("Test Failed!");
+    }
+    
+    driver.close();
+  }
 }
--- src/room_module/TC3.java
+++ src/room_module/TC3.java
@@ -3,7 +3,6 @@ package room_module;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -12,52 +11,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class TC3 {
-	
-	static WebDriver driver;
-	
-	public static void main(String[] args) throws IOException, Exception {
-		
-		System.setProperty("webdriver.chrome.driver", "/......./chromedriver");
-		driver = new ChromeDriver();
-		
-		driver.get("https://www.myguruavatar.com/demo/");
-		
-		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-		
-		WebElement Username = driver.findElement(By.xpath("//*[@id=\"modlgn-username\"]"));
-		Username.sendKeys("josemulamorales33@hotmail.com");
-		
-		WebElement Password = driver.findElement(By.xpath("//*[@id=\"modlgn-passwd\"]"));
-		Password.sendKeys("Jose Morales");
-		
-		WebElement Checkbutton = driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input"));
-		Checkbutton.click();
+  
+  static WebDriver driver;
+  
+  public static void main(String[] args) throws IOException, Exception {
+    System.setProperty("webdriver.chrome.driver", "/......./chromedriver");
+    driver = new ChromeDriver();
+    driver.get("https://www.myguruavatar.com/demo/");
+    driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+    
+    WebElement Username = driver.findElement(By.xpath("//*[@id=\"modlgn-username\"]"));
+    Username.sendKeys("josemulamorales33@hotmail.com");
+    
+    WebElement Password = driver.findElement(By.xpath("//*[@id=\"modlgn-passwd\"]"));
+    Password.sendKeys("Jose Morales");
+    
+    WebElement Checkbutton = driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div/p[1]/input"));
+    Checkbutton.click();
 
-		WebElement Login = driver.findElement(By.xpath("//*[@id=\"hmelogin\"]"));
-		Login.click();
-		
-		Thread.sleep(3000);
-		
-		WebElement Switch = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]"));
-		Switch.click();
-		
-		WebElement Goto = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]/option[2]"));
-		Goto.click();
-		
-		Thread.sleep(6000);
-		
-		takeScreenshot("TC3");
-		
-	}
-	
-	public static void takeScreenshot (String fileName) throws IOException {
-		
-		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		
-		FileUtils.copyFile(file, new File("/Users/jose/Desktop/Software Testing/Selenium/eclipse-workspace/Guru Avatar/Screenshots/"+fileName+".png"));
-		
-	}
-
+    WebElement Login = driver.findElement(By.xpath("//*[@id=\"hmelogin\"]"));
+    Login.click();
+    
+    Thread.sleep(3000);
+    
+    WebElement Switch = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]"));
+    Switch.click();
+    
+    WebElement Goto = driver.findElement(By.xpath("//*[@id=\"swithbtn\"]/option[2]"));
+    Goto.click();
+    
+    Thread.sleep(6000);
+    
+    takeScreenshot("TC3");
+  }
+  
+  public static void takeScreenshot (String fileName) throws IOException {
+    File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    FileUtils.copyFile(file, new File("/Users/jose/Desktop/Software Testing/Selenium/eclipse-workspace/Guru Avatar/Screenshots/"+fileName+".png"));
+  }
 }
